9f6be980aca54f9dde8275b9b00788838e1811ce
Eric Ambiel <eric.ambiel@gmail.com>
2019-12-20 19:45:03 -0300

    Modulo - Activity Principal
     - Implementando: Página inicial com menu em modo gaveta
     - Implementando: Icones, imagem de fundo criadas

---
+++ app/src/main/java/ericambiel/com/br/smartimdb/ui/menuprincipal/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package ericambiel.com.br.smartimdb.ui.menuprincipal.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/ericambiel/com/br/smartimdb/ui/menuprincipal/share/ShareViewModel.kt
@@ -0,0 +1,13 @@
+package ericambiel.com.br.smartimdb.ui.menuprincipal.share
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ShareViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is share Fragment"
+    }
+    val text: LiveData<String> = _text
+}
